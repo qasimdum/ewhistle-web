1826c5a91f58ed3551a3b6e7dd6e7b52f9a5586f
Teimuraz Tutberidze <t.tutberidze@sweeftdigital.com>
2020-06-26 16:24:02 +0400

    config

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export const config = {
-  BASE_URL: 'http://localhost:3030/'
-  //BASE_URL: ' http://52.188.2.39:3030/'
+  //BASE_URL: 'http://localhost:3030/'
+  BASE_URL: ' http://52.188.2.39:3030/'
 };
 
